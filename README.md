4bc52f9637ca95282b49dbead10e0d041e9aae16
Sander <sander.vanbeers@crosslang.com>
2021-03-12 15:05:40 +0100

    add distinction between big corpus and clean corpus during training

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ bash dbuild.sh
 The docker image can be run using the dcli.sh script.
 
 ```
-bash dcli.sh -i name_training_data -v /host/directory/to/bind -s source_language -t target_language
+bash dcli.sh -b name_big_corpus -c name_clean_corpus -v /host/directory/to/bind -s source_language -t target_language
 ```
-The inputfile is expected to be a tab-separated plaintext file containing the source and target segments in different columns.
-The training data should be in the directory you bind to the docker container.
-The source language and target language are expected to be the language codes.
+The big corpus and clean corpus are expected to be a tab-separated plaintext file containing the source and target segments in different columns.
+The corpora should be in the directory you bind to the docker container.
+The source language and target language are expected to be the language codes (ISO 639-1).
--- dcli.sh
+++ dcli.sh
@@ -1,14 +1,15 @@
 #The input data is expected to be in the mapped working directory. Intermediate files will be saved in the container directory /data/
 #Files relevant to the bicleaner instance will be put in the working directory
-while getopts i:v:s:t: flag
+while getopts b:c:v:s:t: flag
 do
     case "${flag}" in
-        i) input_file=${OPTARG};;
+        b) big_corpus=${OPTARG};;
+        c) clean_corpus=${OPTARG};;
         v) working_directory=${OPTARG};;
         s) source_language=${OPTARG};;
         t) target_language=${OPTARG};;
     esac
 done
-docker run --rm -v $working_directory:/workdir:Z bicleaner/training python3 /src/main.py --input $input_file --source $source_language --target $target_language
+docker run --rm -v $working_directory:/workdir:Z bicleaner/training python3 /src/main.py --big_corpus $big_corpus --clean_corpus $clean_corpus --source $source_language --target $target_language
 #Example command:
-#bash dcli.sh -i training_data.de-fr -v /home/sandervanbeers/testbicleanertraining -s de -t fr
+#bash dcli.sh -b big_corpus.de-fr -c clean_corpus -v /home/sandervanbeers/testbicleanertraining -s de -t fr
